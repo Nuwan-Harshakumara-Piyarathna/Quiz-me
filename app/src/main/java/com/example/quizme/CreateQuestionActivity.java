1621d0f13d16dedb04193c899d64a9d321e53e52
NuwanHarsha <nuwan.harshamatrix@gmail.com>
2021-06-19 08:56:17 +0530

    minor changes

--- app/src/main/java/com/example/quizme/CreateQuestionActivity.java
+++ app/src/main/java/com/example/quizme/CreateQuestionActivity.java
@@ -1,14 +1,7 @@
 package com.example.quizme;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -16,7 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateQuestionActivity extends AppCompatActivity {
 
