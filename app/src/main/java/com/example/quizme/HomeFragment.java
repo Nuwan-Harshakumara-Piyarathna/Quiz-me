84d53665e3a5240bf44d4a57e5d370ae6f6ec726
Erandana <erandanawijerathne@gmail.com>
2021-06-30 09:02:06 -0700

    past quizzes ui added

--- app/src/main/java/com/example/quizme/HomeFragment.java
+++ app/src/main/java/com/example/quizme/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.quizme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,7 +16,17 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         ViewGroup homeFrag = (ViewGroup)inflater.inflate(R.layout.home_fragment, container, false);
+        Button qBtn = homeFrag.findViewById(R.id.quizTake);
+        qBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), PastQuizActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
         return  homeFrag;
     }
 }
